72ccfe3c9b90e53c2e195a217c55c240e6279d99
apesteguia <mikeltxomikeltxo@gmail.com>
2025-11-08 16:45:45 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir ~/.config/alacritty/
+cp alacritty.toml ~/.config/alacritty/
+
+mkdir ~/.config/ghostty/
+cp config ~/.config/ghostty/
+
+cp mikel.zsh-theme ~/.oh-my-zsh/themes/
+
+cp .tmux.conf ~/
